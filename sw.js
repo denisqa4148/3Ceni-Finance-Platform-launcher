b693a685cdd411624bf5a1d9997d52b333007f2e
denisqa4148 <154381562+denisqa4148@users.noreply.github.com>
2026-09-03 09:56:18 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,12 @@
-const CACHE = 'tri-ceny-finance-launcher-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
+const CACHE = 'tri-ceny-finance-launcher-v3';
+const STATIC = [
   './icons/apple-touch-icon.png',
   './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icons/icon-512.png',
+  './assets/install-guide.mp4'
 ];
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)));
   self.skipWaiting();
 });
 self.addEventListener('activate', event => {
@@ -17,5 +15,13 @@ self.addEventListener('activate', event => {
 });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).then(r => r).catch(() => caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(resp => {
+    const copy = resp.clone();
+    caches.open(CACHE).then(c => c.put(event.request, copy));
+    return resp;
+  })));
 });
